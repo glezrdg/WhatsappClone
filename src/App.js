954e367a0020f1d596795873adb6b00105595a71
German <glezrdg@gmail.com>
2023-01-05 02:55:14 -0400

    aplicando algunos estilos ya esta casi terminado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import Sidebar from './components/Sidebar'
-import Chat from './components/Chat'
-
-import './App.css';
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+import "./App.css";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
-    <div className="app">
-      <div className="app__body">
-        <Sidebar />
-        <Chat />
-      </div>  
+    <div className="app ">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCwNcbw6bl5jRZBd79j-K19P-YKgfqTnoQ",
-  authDomain: "whatsapp-clone-268fc.firebaseapp.com",
-  projectId: "whatsapp-clone-268fc",
-  storageBucket: "whatsapp-clone-268fc.appspot.com",
-  messagingSenderId: "99137607220",
-  appId: "1:99137607220:web:e4eedf4ef2eaad68badc61",
-  measurementId: "G-MZVXFWY4GJ"
+  apiKey: "AIzaSyCpTlzg6RAdfnzQX8A-sXnbajHSCOH0BH4",
+  authDomain: "whatsapp-clone-fcd3f.firebaseapp.com",
+  projectId: "whatsapp-clone-fcd3f",
+  storageBucket: "whatsapp-clone-fcd3f.appspot.com",
+  messagingSenderId: "359761166573",
+  appId: "1:359761166573:web:6100b3ee972149f0734ce4",
+  measurementId: "G-YVBXCLNMD2",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
+
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
